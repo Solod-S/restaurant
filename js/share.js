1a98b4dec8b2ef5c20c069b0786a0f11ec017e91
Solod Sergey <Solod098@gmail.com>
2022-09-07 16:58:01 +0300

    corection share menu

--- js/share.js
+++ js/share.js
@@ -6,14 +6,25 @@ const share = {
   body: document.querySelector('body'),
   onKeyPresEsq(event) {
     if (event.code === 'Escape') {
-      share.modal.classList.toggle('is-hidden');
+      share.modal.classList.add('is-hidden');
       share.body.classList.toggle('no-scroll');
+      this.closed = true;
       window.removeEventListener('keydown', share.onKeyPresEsq);
     }
   },
-  toggleModal() {
+  // toggleModal() {
+  //   window.addEventListener('keydown', share.onKeyPresEsq);
+  //   this.modal.classList.toggle('is-hidden');
+  //   this.body.classList.toggle('no-scroll');
+  // },
+  closeModel() {
+    window.removeEventListener('keydown', share.onKeyPresEsq);
+    this.modal.classList.add('is-hidden');
+    this.body.classList.toggle('no-scroll');
+  },
+  openModel() {
     window.addEventListener('keydown', share.onKeyPresEsq);
-    this.modal.classList.toggle('is-hidden');
+    this.modal.classList.remove('is-hidden');
     this.body.classList.toggle('no-scroll');
   },
   closeOnTargetClick(event) {
@@ -23,12 +34,12 @@ const share = {
     // // используем чтобы точно указать что выбираем
 
     if (event.currentTarget === event.target) {
-      this.modal.classList.toggle('is-hidden');
+      this.modal.classList.add('is-hidden');
       this.body.classList.toggle('no-scroll');
     }
   },
 };
 
-share.closeModalBtn.addEventListener('click', share.toggleModal.bind(share));
+share.closeModalBtn.addEventListener('click', share.closeModel.bind(share));
 share.modal.addEventListener('click', share.closeOnTargetClick.bind(share));
-setTimeout(share.toggleModal.bind(share), 3000, 3);
+setTimeout(share.openModel.bind(share), 3000, 3);
